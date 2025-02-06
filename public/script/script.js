b83ef51ad6886bc27152fe897bf338786381a2cc
MarsGotBars <marcinm@live.nl>
2025-02-06 23:28:58 +0100

    pushin
    lots of changes

--- public/script/script.js
+++ public/script/script.js
@@ -1,15 +1,49 @@
-const toggleDisplay = document.querySelector('#toggle');
-const mainEl = document.querySelector('main');
+const toggleDisplay = document.querySelector("#toggle");
+const mainEl = document.querySelector("main");
+const hobbyList = document.querySelector(".slider");
 
-toggleDisplay.addEventListener('click', ()=>{
-    showContent()
-})
-toggleDisplay.addEventListener('touch', ()=>{
-    showContent()
-    console.log('tap');
-    
-})
+if (toggleDisplay) {
+  toggleDisplay.addEventListener("click", () => {
+    showContent();
+  });
 
-const showContent = () => {
-    mainEl.classList.toggle("fold")
+  toggleDisplay.addEventListener("touch", () => {
+    showContent();
+  });
+}
+
+// on page load, remove the transition and close the main (in case of no JS)
+const showContent = (param) => {
+  param
+    ? mainEl.classList.add("no-animation")
+    : mainEl.classList.remove("no-animation");
+  mainEl.classList.toggle("fold");
+};
+
+if (mainEl) {
+  // toggle fold on load
+  showContent(true);
+}
+
+// starts at one because forloop.index starts at 1 for some reason
+let currentSlide = 1;
+
+function changeSlide() {
+  const numHobbies = hobbyList.children.length;
+
+  // Remove the "active" class from all items
+  Array.from(hobbyList.children).forEach((item) => {
+    item.classList.remove("active");
+  });
+
+  // Add "active" class to the current slide
+  hobbyList.children[currentSlide].classList.add("active");
+
+  // Update currentSlide for the next interval
+  currentSlide = currentSlide === numHobbies - 1 ? 0 : currentSlide + 1;
+}
+
+if (hobbyList) {
+  // Start the interval
+  const intervalId = setInterval(changeSlide, 3000);
 }
